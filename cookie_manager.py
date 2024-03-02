260ff955ec75b263945d472f80879a4dd01ba69a
Grigoriy Temnikov <GrigST45@mail.ru>
2024-03-02 16:05:48 +0500

    Fixed error after removing account

--- cookie_manager.py
+++ cookie_manager.py
@@ -9,25 +9,26 @@ class CookieManager:
         self.filename = filename
         self.cookie_jars = cookie_jars
         self.save_enabled = save_enabled
+        self.cookies = {}
 
     def load(self):
         if not os.path.exists(self.filename):
             return
         
         with open(self.filename, 'r') as f:
-            cookies = json.load(f)
+            self.cookies = json.load(f)
 
-        for username, user_cookies in cookies.items():
-            for cookie in user_cookies:
-                self.cookie_jars[username].set(**cookie)
+        for username, user_cookies in self.cookies.items():
+            if username in self.cookie_jars:
+                for cookie in user_cookies:
+                    self.cookie_jars[username].set(**cookie)
 
     def save(self):
         if not self.save_enabled:
             return
         
-        cookies = {}
         for username, user_cookies in self.cookie_jars.items():
-            cookies[username] = [{
+            self.cookies[username] = [{
                 "name": c.name,
                 "value": c.value,
                 "domain": c.domain,
@@ -36,4 +37,4 @@ class CookieManager:
             } for c in user_cookies]
 
         with open(self.filename, 'w') as f:
-            json.dump(cookies, f)
+            json.dump(self.cookies, f)
